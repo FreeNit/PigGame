f6229b0dbb0c3305da4fa0c9cfe87f4e5c72a0fc
FreeNit <bolilui.dmytro@gmail.com>
2019-02-28 16:51:05 +0200

    Implementation "Hold" functionality

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ GAME RULES:
 var scores, roundScore, activePlayer;
 
 scores = [0, 0];
-roundScrore = 0;
-activePlayer = 1;
+roundScore = 0;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -25,8 +25,8 @@ document.getElementById('current-1').textContent = '0';
 document.querySelector('.btn-roll').addEventListener('click', function(){
 
     // 1. Random number
-    dice = Math.floor(Math.random() * 6) + 1;
-
+    var dice = Math.floor(Math.random() * 6) + 1;
+    
     // 2. Display the result
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
@@ -34,11 +34,51 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 
 
     // 3. Updat the round score IF the rolled number was NOT a 1
+    if(dice !== 1){
+        //Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        nextPlayer();
+    }
+});
 
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    // 1. Add CURRENT score to GLOBAL score
+    scores[activePlayer] += roundScore;
+
+    // 2. Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    // 3. Check if player won the game
+    if(scores[activePlayer] >= 100){
+        document.querySelector('#name-' + activePlayer).textContent = "Winner!";
+    } else {
+        
+    }
+
+
+    //Next player
+    nextPlayer();
 });
 
 
+function nextPlayer() {
+     //Next player
+     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+     roundScore = 0;
 
+     document.getElementById('current-0').textContent = '0';
+     document.getElementById('current-1').textContent = '0';
+
+     //document.querySelector('.player-0-panel').classList.remove('active');
+     //document.querySelector('.player-1-panel').classList.add('active');
+     
+     document.querySelector('.player-0-panel').classList.toggle('active');
+     document.querySelector('.player-1-panel').classList.toggle('active');
+
+     document.querySelector('.dice').style.display = 'none';
+}
 
 
 
